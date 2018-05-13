7b81227b2b152bfcd9a1d9f435fe80a5fd52e0d7
Unknown <tomsonrz@protonmail.ch>
2018-05-13 19:31:39 +0200

    eval

--- plugins/eval.rb
+++ plugins/eval.rb
@@ -5,18 +5,36 @@ module Eval
     break unless event.user.id == 346288821115289602
 
     begin
-	event.channel.send_embed do |e|
-	e.title = '**Eval**'
+      event.channel.send_embed do |e|
+        e.title = '**Eval**'
       evaluated = eval code.join(' ')
 	  puts '--BEGIN EVAL--'
 	  puts evaluated.to_s
 	  puts '--END EVAL--'
 	  e.description = evaluated.to_s
 	  e.color = '00ff00'
+    end
+    rescue StandardError => f
+      event.channel.send_embed do |e|
+        e.title = '**Evaluation Failed!**'
+
+        e.description = f.to_s
+        e.color = 'FF0000'
+        end
+    end
+  end
+
+  $bot.command(:s_eval) do |event, *code|
+    break unless event.user.id == 346288821115289602
+
+    begin
+      evaluated = eval code.join(' ')
+      puts '--BEGIN EVAL--'
+      puts evaluated
+      puts '--END EVAL--'
+      event.send_message evaluated
     rescue
-	
-    e.description = '**Error while evaluating!**'
-	e.color = 'FF0000'
+      '**Error while evaluating!**'
     end
-      end
-    end
+  end
+end
