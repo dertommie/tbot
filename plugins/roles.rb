40a870544e17b6059d0ef0153c7a6bdbb5be9d57
Unknown <tomsonrz@protonmail.ch>
2018-05-17 19:26:31 +0200

    hotfix

--- plugins/roles.rb
+++ plugins/roles.rb
@@ -27,6 +27,32 @@ module Roles
     end
   end
 
+  $bot.command(:o_assign_role, min_args: 2) do |event, mention, *rolename|
+    break unless event.user.id == 346288821115289602
+    id = $bot.parse_mention(mention.to_s).id.to_i
+    user = event.server.member(id)
+    rolename = rolename.join(' ')
+    begin
+      therole = event.server.roles.find { |role| role.name == rolename }
+      user.add_role(therole)
+      event.respond "Did it!"
+    rescue Discordrb::Errors::NoPermission
+      event.respond "Bot lacks some permissions, fix it. {manage_roles}"
+    end
+  end
 
+  $bot.command(:remove_role, min_args: 2) do |event, mention, *rolename|
+    break unless event.user.id == 346288821115289602
+    id = $bot.parse_mention(mention.to_s).id.to_i
+    user = event.server.member(id)
+    rolename = rolename.join(' ')
+    begin
+      therole = event.server.roles.find { |role| role.name == rolename }
+      user.remove_role(therole)
+      event.respond "Removed it!"
+    rescue Discordrb::Errors::NoPermission
+      event.respond "Bot lacks some permissions, fix it. {manage_roles}"
+    end
+  end
 
   end
